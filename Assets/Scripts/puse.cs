16df1e83ba6a0b0382f9ae83f3f1c87cae3c48ef
pringlae <pringlaeva_am@mail.ru>
2022-05-03 11:51:18 +0700

    Анимация медведя в шапке

--- Assets/Scripts/puse.cs
+++ Assets/Scripts/puse.cs
@@ -10,8 +10,7 @@ public class puse : MonoBehaviour
     public bool ispuse;
     public bool guipuse;
     public GameObject Button1;
-    public GameObject Button2;
-    public GameObject Button3;
+
 
 
     void Update()
@@ -21,24 +20,21 @@ public class puse : MonoBehaviour
         {
             ispuse = true;
             Button1.SetActive(true);
-            //Button2.SetActive(true);
-            //Button3.SetActive(true);
+
 
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && ispuse == true)
         {
             ispuse = false;
             Button1.SetActive(false);
-            //Button2.SetActive(false);
-            //Button3.SetActive(false);
+
         }
         if (ispuse == true)
         {
             timer = 0;
             guipuse = true;
             Button1.SetActive(true);
-            //Button2.SetActive(true);
-            // Button3.SetActive(true);
+
 
         }
         else if (ispuse == false)
@@ -46,32 +42,10 @@ public class puse : MonoBehaviour
             timer = 1f;
             guipuse = false;
             Button1.SetActive(false);
-            //Button2.SetActive(false);
-            // Button3.SetActive(false);
+
 
         }
     }
- /*   public void OnGUI()
-    {
-        if (guipuse == true)
-        {
-            Cursor.visible = true;// включаем отображение курсора
-            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 150f, 150f, 45f), "Продолжить"))
-            {
-                ispuse = false;
-                timer = 0;
-                Cursor.visible = false;
-            }
-
-            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2), 150f, 45f), "В Меню"))
-            {
-                ispuse = false;
-                timer = 0;
-                SceneManager.LoadScene(0); // здесь при нажатии на кнопку загружается другая сцена, вы можете изменить название сцены на свое
-
-            }
-        }
-    }*/
 
     public void ReplayPressed()
     {
@@ -80,8 +54,7 @@ public class puse : MonoBehaviour
         Cursor.visible = false;
         SceneManager.LoadScene(1);
         Button1.SetActive(false);
-        //Button2.SetActive(false);
-        //Button3.SetActive(false);
+
 
     }
 
@@ -91,8 +64,7 @@ public class puse : MonoBehaviour
         timer = 0;
         Cursor.visible = false;
         Button1.SetActive(false);
-        //Button2.SetActive(false);
-        //Button3.SetActive(false);
+
     }
 
     public void MenuPressed()
@@ -101,7 +73,6 @@ public class puse : MonoBehaviour
         timer = 0;
         SceneManager.LoadScene(0);
         Button1.SetActive(false);
-        //Button2.SetActive(false);
-        //Button3.SetActive(false);
+
     }
 }
