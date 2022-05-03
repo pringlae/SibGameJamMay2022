bcac4ab1a3bbef2756233edda641a3c0c5a715f3
CORRUPTOR2037 <glados.ver17.dmw@gmail.com>
2022-05-03 10:43:14 +0300

    Аниматор деревьев

---
+++ Assets/Scripts/TreeAnimator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAnimator : MonoBehaviour
+{
+    public float frameLength;
+    public Sprite[] frames;
+
+    private new SpriteRenderer renderer;
+    private float untilNext;
+    private int currentFrame;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        untilNext = frameLength;
+        currentFrame = 0;
+        renderer.sprite = frames[currentFrame];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        untilNext -= Time.deltaTime;
+        if (untilNext < 0)
+        {
+            untilNext += frameLength;
+            currentFrame = (currentFrame + 1) % frames.Length;
+            renderer.sprite = frames[currentFrame];
+        }
+    }
+}
