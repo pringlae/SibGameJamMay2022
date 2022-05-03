1967adbd5c6fbd1c1e34c374c8ddf4700f3e8bea
CORRUPTOR2037 <glados.ver17.dmw@gmail.com>
2022-05-03 18:00:43 +0300

    Квест бобра

--- Assets/Scripts/SceneInteractions/Beaver.cs
+++ Assets/Scripts/SceneInteractions/Beaver.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Beaver : Animal
 {
+    public List<SceneInteraction> logs = new List<SceneInteraction>();
+
+    private int logsNeeded;
+
+    void Start()
+    {
+        logsNeeded = logs.Count;
+    }
+    
     public override void OnDayStart(int dayIndex)
     {
-        
+        if (logsNeeded > 0)
+            Activate();
     }
-    public override void OnDayEnd(int dayIndex)
+
+    protected override void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (!enabled || other.gameObject.layer != 6) return;
+
+        var bear = other.GetComponent<BearMovement>();
+        if (bear.itemHeld == null || !logs.Contains(bear.itemHeld) || !bear.itemOnHand) return;
+
+        SetNearBy();
+    }
+
+    public override void OnUse(BearMovement bear)
+    {
+        logsNeeded--;
+        logs.Remove(bear.itemHeld);
+        bear.ClearItem();
+        if (logsNeeded <= 0)
+            base.OnUse(bear);
     }
 }
